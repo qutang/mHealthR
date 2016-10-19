1ea36460633096477bab4f07602d3c7ae4e10799
Qu Tang <tqshelly@gmail.com>
2016-10-19 11:22:14 -0400

    Refactor mhealth.plot to use group_cols

--- R/mhealth.plot.R
+++ R/mhealth.plot.R
@@ -1,8 +1,5 @@
 #' @name mhealth.plot
 #' @title plot or add to plot of dataframe of different file types in mhealth specification
-#' @param size "thumbnail", "poster", "", "publication"
-#' @param aspect_ratio width/height
-#' @param interactive whether to support zoom in, scrolling, tooltip
 #' @export
 #' @import ggplot2 reshape2
 
@@ -10,14 +7,15 @@ mhealth.plot = function(p = NULL,
                         df,
                         file_type,
                         interactive = FALSE,
-                        divide_by = NULL) {
-  valid = mhealth.validate(df, file_type = file_type, group_cols = divide_by)
+                        group_cols = NULL,
+                        ncols = 4) {
+  valid = mhealth.validate(df, file_type = file_type, group_cols = group_cols)
   if (!valid)
     return(NULL)
   if (file_type == mhealth$filetype$sensor) {
     id = c(mhealth$column$TIMESTAMP)
-    if (is.character(divide_by)) {
-      id = c(id, divide_by)
+    if (is.character(group_cols)) {
+      id = c(id, group_cols)
     }
     md = reshape2::melt(df, id = id, variable = "series")
     p = .plot.point_numeric(p, md, interactive)
@@ -27,16 +25,16 @@ mhealth.plot = function(p = NULL,
     }, simplify = TRUE)
     cols = c(1, numeric_cols)
     id = c(mhealth$column$TIMESTAMP)
-    if (is.character(divide_by)) {
-      cols = c(cols, which(names(df) == divide_by))
-      id = c(id, divide_by)
+    if (is.character(group_cols)) {
+      cols = c(cols, which(names(df) == group_cols))
+      id = c(id, group_cols)
     }
     df_num = df[cols]
     md = reshape2::melt(df_num, id = id, variable = "series")
     p = .plot.range_numeric(p, md, interactive)
     # deal with categorical columns
   } else if (file_type == mhealth$filetype$annotation) {
-    cols = c(2,3,4, which(names(df) == divide_by))
+    cols = c(2, 3, 4, which(names(df) == group_cols))
     df_range = df[cols]
     p = .plot.range_categoric(p, df_range, interactive)
   }
@@ -44,9 +42,14 @@ mhealth.plot = function(p = NULL,
   p = p + theme_bw(base_size = 9)
   p = p + theme(legend.position = "top")
   p = p + xlab(label = "") + ylab(label = "")
-  if (is.character(divide_by)) {
-    p = p + theme(axis.text.x=element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), strip.background = element_blank())
-    p = p + facet_wrap(divide_by, ncol = 4, scales = "free_x")
+  if (is.character(group_cols)) {
+    p = p + theme(
+      axis.text.x = element_blank(),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      strip.background = element_blank()
+    )
+    p = p + facet_wrap(group_cols, ncol = ncols, scales = "free_x")
   }
 
   return(p)
@@ -71,10 +74,10 @@ mhealth.plot = function(p = NULL,
 
 .plot.range_categoric = function(p, df, interactive, jitter = TRUE) {
   categories = unique(df[, 3])
-  if(jitter){
+  if (jitter) {
     ypos = jitter(rep(0, length(categories)), amount = length(categories) /
                     3)
-  }else{
+  } else{
     ypos = rep(0, length(categories))
   }
 
