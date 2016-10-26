657244451d7dd9ad338b85a58af8b6c9ff5e299a
Qu Tang <qutang@Qus-MacBook-Air.local>
2016-10-26 16:03:04 -0400

    Update to version 0.5.6, add visualization for feature bar plot for each instance(row)
    Update website

--- R/mhealth.plot.R
+++ R/mhealth.plot.R
@@ -24,11 +24,12 @@ mhealth.plot_timeseries <- function(dfs,
   if (length(select_cols) > 1) {
     stopifnot(is.list(select_cols))
   }
-  stopifnot(length(dfs) >= length(file_types) &&
-              length(dfs) >= length(select_cols))
+  stopifnot(length(dfs) != length(file_types) &&
+              length(dfs) != length(select_cols))
 
   n_total = length(dfs)
 
+  # Find out the common Y scale
   v_max = c()
   v_min = c()
   for (i in 1:n_total) {
@@ -45,6 +46,7 @@ mhealth.plot_timeseries <- function(dfs,
   }
 
 
+  # Get the original time zone
   tz = lubridate::tz(dfs[[1]][1, 1])
 
   if (is.null(group_cols)) {
@@ -150,6 +152,71 @@ mhealth.plot_timeseries <- function(dfs,
   }
 }
 
+#' @name mhealth.plot_instance
+#' @title Plot instance (each row in a feature data file) with bar chart, so taht easy to analyze and compare features for different instances.
+#' @param df feature set file
+#' @param select_cols selected cols for input data frame to be plotted.
+#' @param group_cols group cols that contains extra information about the row to be displayed
+#' @param ncols number of columns in the subplot layout per page. Default is 4 columns.
+#' @param nrows number of rows in the subplot layout per page. Default is NULL, which will be calculated automatically by `ncols`, so that ncols * nrows >= total number of subplots. But it should not exceed 6 per page.
+#' @export
+#' @import ggplot2 reshape2 plyr gridExtra
+
+mhealth.plot_instance <- function(df,
+                                   select_cols,
+                                   group_cols = NULL,
+                                   ncols = 4,
+                                   nrows = NULL) {
+  # validate input arguments
+
+  s_cols = .convert.column_input(df, select_cols)
+  g_cols = .convert.column_input(df, group_cols)
+  if(mhealth.validate(df, file_type = "feature")){
+    df_standard = df
+    df_standard[s_cols] = lapply(df[s_cols], function(col_data){
+      result = (col_data - mean(col_data, na.rm = TRUE)) / sd(col_data, na.rm = TRUE)
+      return(result)
+    })
+  }
+  y_min = floor(min(df_standard[s_cols]))
+  y_max = ceiling(max(df_standard[s_cols]))
+  p_list = lapply(1:nrow(df), function(i){
+    title_label = ""
+    if(!is.null(g_cols)){
+      title_label = stringr::str_c(df[i, g_cols], collapse = "-")
+    }
+    xlabel = sprintf("%s - %s",
+                     format(df[i, 2], "%H:%M:%OS"),
+                     format(df[i, 3], "%H:%M:%OS"))
+    p = ggplot()
+    p = .plot.numeric_row(p = p, df = df[i,c(1,2,3,s_cols)], df_standard = df_standard[i, c(1,2,3,s_cols)])
+    p = p + ggtitle(title_label) + ylab("") + xlab(xlabel)
+    p = p + ylim(y_min, y_max)
+    p <- p + theme_bw(base_size = 9)
+    p <- p + theme(
+      legend.position = "top",
+      axis.title.x = element_text(size = 5.5),
+      title = element_text(size = 5.5),
+      axis.text.x = element_text(size = 3, angle = 30),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      strip.background = element_blank()
+    )
+    return(p)
+  })
+  if(is.null(nrows)){
+    nrows = min(ceiling(length(p_list)) / ncols, 6)
+  }else{
+    nrows = min(nrows, 6)
+  }
+
+  return(gridExtra::marrangeGrob(
+    p_list,
+    ncol = ncols,
+    nrow = nrows
+  ))
+}
+
 .plot.timeseries = function(p, df, select_cols, file_type, range = NULL) {
   s_cols = .convert.column_input(df, select_cols)
 
@@ -239,5 +306,48 @@ mhealth.plot_timeseries <- function(dfs,
     hjust = 0,
     vjust = 0
   )
+  return(p)
+}
+
+.plot.numeric_row = function(p, df, df_standard) {
+  df <- reshape2::melt(
+    df,
+    id = c(mhealth$column$TIMESTAMP, mhealth$column$START_TIME, mhealth$column$STOP_TIME),
+    variable.name = "category",
+    value.name = "value"
+  )
+
+  df$value = round(df$value * 100) / 100
+
+  df_standard <- reshape2::melt(
+    df_standard,
+    id = c(mhealth$column$TIMESTAMP, mhealth$column$START_TIME, mhealth$column$STOP_TIME),
+    variable.name = "category",
+    value.name = "value"
+  )
+
+  df_standard$value = round(df_standard$value * 100) / 100
+
+  df$standard_value = df_standard$value
+  remove(df_standard)
+
+  df$vjust = df$standard_value < 0
+
+  p <- p + geom_bar(data = df,
+                     aes_string(
+                       x = "category",
+                       y = "standard_value"
+                     ), stat = "identity")
+  p = p + geom_text(data = df,
+                    aes_string(
+                      x = "category",
+                      y = "standard_value",
+                      label = "value",
+                      vjust = "vjust"
+                    ),
+                    stat = "identity",
+                    size = 2,
+                    hjust = 0.5)
+
   return(p)
 }
